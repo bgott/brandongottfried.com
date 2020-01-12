8911b674bf6dfea0de14e6e9e4cc455ce56713c4
Brandon Gottfried <bgott@stanford.edu>
2020-01-11 18:34:27 -0800

    point 404 at patientlink, delete elements

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import Layout from '../components/layout'
+import PatientLink from '../pages/patient-link'
 
 const NotFoundPage = () => (
-  <Layout>
-    <div id="main" className="alt">
-        <section id="one">
-            <div className="inner">
-                <h1>NOT FOUND</h1>
-                <p>Sorry, the page you were looking for could not be found. The page may have moved or it no longer exists.</p>
-                <p>Try checking the URL or return home.</p>
-            </div>
-        </section>
-    </div>
-  </Layout>
+  <PatientLink />
 )
 
 export default NotFoundPage
